b4a05a270688356cb2c8c6625e99c8f9a38a8d76
Aditya <48057984+adityaj0220@users.noreply.github.com>
2020-06-17 13:04:46 +0530

    Update user.js

--- backend/contractInstances/user.js
+++ backend/contractInstances/user.js
@@ -3,6 +3,6 @@ import User from '../build/User.json';
 
 const instance = new web3.eth.Contract(
     User.abi,
-    '0xdCadb4E7783b297b1a0A3cD7c01C126808aD4c7e'
+    'Deployed Address'
 );
 export default instance;
